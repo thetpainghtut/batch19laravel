8d6215e0f0fabe729557236b46b4b3b5bc6fcb04
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-12-24 12:05:05 +0630

    api flow

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        // dd($request); // var_dump();, die();
+
+        // validation
+        $request->validate([
+            'name' => 'required|min:5',
+            'photo' => 'required|mimes:jpeg,jpg,png' 
+        ]);
+
+        // upload
+        if($request->file()) {
+            // 624872374523_a.jpg
+            $fileName = time().'_'.$request->photo->getClientOriginalName();
+
+            // categoryimg/624872374523_a.jpg
+            $filePath = $request->file('photo')->storeAs('categoryimg', $fileName, 'public');
+
+            $path = '/storage/'.$filePath;
+        }
+
+        // store data
+        $category = new Category;
+        $category->name = $request->name;
+        $category->photo = $path;
+        $category->save();
+
+        // redirect
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        // dd($request); // var_dump();, die();
+
+        // validation
+        $request->validate([
+            'name' => 'required|min:5',
+            'photo' => 'sometimes|mimes:jpeg,jpg,png' 
+        ]);
+
+        // upload
+        if($request->file()) {
+            // 624872374523_a.jpg
+            $fileName = time().'_'.$request->photo->getClientOriginalName();
+
+            // categoryimg/624872374523_a.jpg
+            $filePath = $request->file('photo')->storeAs('categoryimg', $fileName, 'public');
+
+            $path = '/storage/'.$filePath;
+
+            // delete old image
+            
+            $category->photo = $path;
+        }
+
+        // update data
+        $category->name = $request->name;
+        $category->save();
+
+        // redirect
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return new CategoryResource($category);
+    }
+}
